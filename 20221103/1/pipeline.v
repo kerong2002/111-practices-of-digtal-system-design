026597a786af177d6bf9b707c2e49cc3f4e30ee5
kerong <70834651+kerong2002@users.noreply.github.com>
2022-11-03 17:42:52 +0800

    Add files via upload

--- 20221103/1/pipeline.v
+++ 20221103/1/pipeline.v
@@ -1,51 +1,81 @@
-module pipeline(clk,rst,a,b,c,d,Y);
-input clk,rst;
-input [5:0] a,b,c,d;
-output reg[7:0] Y;
+module pipeline(clk, rst, a, b, c, d, Y);
+	input clk, rst;
+	input [5:0] a, b, c, d;
+	output reg[7:0] Y;
+	reg [5:0] reg_a, reg_b, reg_c;
+	reg [11:0] reg_d;
+	reg [6:0] next_sum_abc;
+	reg [6:0] next_carry_abc;
+	reg [6:0] reg_sum_abc;
+	reg [6:0] reg_carry_abc;
+	reg [7:0] next_sum_d;
+	reg [7:0] next_carry_d;
+	reg [7:0] reg_sum_d;
+	reg [7:0] reg_carry_d;
 
-reg [6:0] s1;
-reg [6:0] c1;
-reg [7:0] s2;
-reg [7:0] c2;
-reg [5:0] save_d;
-always @(posedge clk,posedge rst)begin
-	if(rst)begin
-		Y <= 8'd0;
-	end
-	else begin
-		save_d <= d;
-		Y <= (c2 + s2);
-	end
-end
-//=========<第一級流水>==========
-integer x;
-always @(posedge clk,posedge rst)begin
-	if(rst)begin
-		c1 <= 7'd0;
-		s1 <= 7'd0;
-	end
-	else begin
-		s1[6] <= 1'b0;
-		c1[0] <= 1'b0;//>>>
+	integer x;
+	
+	always @(*)begin : adder_abc
+		next_sum_abc[6] = 1'b0;
+		next_carry_abc[0] = 1'b0;
 		for(x=0;x<6;x=x+1)begin
-			{c1[x+1],s1[x]} <= a[x] + b[x] + c[x];
+			{next_carry_abc[x+1],next_sum_abc[x]} = reg_a[x] + reg_b[x] + reg_c[x];
 		end
 	end
-end 
-//=========<第二級流水>==========
-always @(posedge clk,posedge rst)begin
-	if(rst)begin
-		c2 <= 8'd0;
-		s2 <= 8'd0;
-	end
-	else begin
-		s2[7] <= 1'b0;
-		c2[0] <= 1'b0;
+	
+	always @(*)begin : adder_d
+		next_sum_d[7] = 1'b0;
+		next_carry_d[0] = 1'b0;
 		for(x=0;x<6;x=x+1)begin
-			{c2[x+1],s2[x]} <= c1[x] + s1[x] + save_d[x]; 
+			{next_carry_d[x+1],next_sum_d[x]} = reg_carry_abc[x] + reg_sum_abc[x] + reg_d[x]; 
 		end
-		{c2[7],s2[6]} <= c1[6] + s1[6];
+		{next_carry_d[7],next_sum_d[6]} = reg_carry_abc[6] + reg_sum_abc[6];
 	end
-end 
+	
+	always @(posedge clk or posedge rst)begin : pipeline1
+		if(rst)begin
+			reg_a <= 6'd0;
+			reg_b <= 6'd0;
+			reg_c <= 6'd0;
+			reg_d <= 12'd0;
+		end
+		else begin
+			reg_a <= a;
+			reg_b <= b;
+			reg_c <= c;
+			reg_d <= {d,reg_d[11:6]};
+		end
+	end
+	
+	always @(posedge clk or posedge rst)begin : pipeline2
+		if(rst)begin
+			reg_sum_abc   <= 7'd0;
+			reg_carry_abc <= 7'd0;
+		end
+		else begin
+			reg_sum_abc   <= next_sum_abc;
+			reg_carry_abc <= next_carry_abc;
+		end
+	end
+	
+	always @(posedge clk or posedge rst)begin : pipeline3
+		if(rst)begin
+			reg_sum_d   <= 8'd0;
+			reg_carry_d <= 8'd0;
+		end
+		else begin
+			reg_carry_d   <= next_carry_d;
+			reg_sum_d     <= next_sum_d;
+		end
+	end
+	
+	always @(posedge clk or posedge rst)begin : pipeline4
+		if(rst)begin
+			Y <= 8'd0;
+		end
+		else begin
+			Y <= reg_sum_d + reg_carry_d;
+		end
+	end
 
 endmodule 
