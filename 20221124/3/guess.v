d269ad456af107ec6f4d328cd96a59ddbac3d67b
kerong <70834651+kerong2002@users.noreply.github.com>
2022-11-25 09:27:21 +0800

    Add files via upload

--- 20221124/3/guess.v
+++ 20221124/3/guess.v
@@ -7,48 +7,47 @@
 `define A1 answer[7:4]
 `define A0 answer[3:0]
 
-module guess(clk, rst, SW, HEX7, HEX6, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0, KEY, LEDR, state, pass_guess, A_cnt, guess_cnt,B_cnt);
+module guess(clk, SW, HEX7, HEX6, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0, KEY, LEDR, LEDG);
 	input clk;									
-	input rst;
-	input [17:0]SW;
-	input [3:0] KEY;
+	input 	  [17:0]SW;
+	input 	  [3:0] KEY;
 	output reg [6:0] HEX7, HEX6, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0;
-	output [17:0] LEDR;	
-	reg [15:0] answer;	
-	
-	output reg [1:0] state;
+	wire  	  [6:0] switch0, switch1, switch2, switch3, switch5, switch7;
+	output 	  [17:0] LEDR;	
+	output     [3:0]  LEDG;
+	reg 		  [15:0] answer;	
+	reg [1:0] state;
 	reg [1:0] nextstate;
-	wire [6:0] switch0;
-	wire [6:0] switch1;
-	wire [6:0] switch2;
-	wire [6:0] switch3;
-	wire [6:0] switch5;
-	wire [6:0] switch7;
+	
 	parameter [1:0] SET   = 2'd0;
 	parameter [1:0] WAIT  = 2'd1;
 	parameter [1:0] GUESS = 2'd2;
 	parameter [1:0] END 	 = 2'd3;
 	
-	//output KEY_3;
-	output pass_guess;
-	output reg [17:0] guess_cnt;
+	wire pass_guess;
+	reg [17:0] guess_cnt;
 	reg [6:0] seg7_run_cycle;
-	output reg [3:0] A_cnt;
-	output reg [3:0] B_cnt;
-	assign pass_guess = (A_cnt == 4'd4);
+	reg [3:0] A_cnt;
+	reg [3:0] B_cnt;
+	
 	wire clk18;
-	assign clk18 = clk;
-	/*
+	wire KEY_3;
+	wire KEY_take;
+	wire rst;
+	assign rst =  KEY[0];
 	counterDivider #(22, 50000000/18) cntD(clk, rst, clk18);
-	KEY_Debounce d2(clk18, rst, KEY[3], KEY_3);
-   */
+	KEY_Debounce d2(clk18, rst, KEY[3], KEY_take);
+	/*
+	assign KEY_3 = KEY[3];
+	assign clk18 = clk;
+	*/
+	assign LEDR = guess_cnt;
+	assign pass_guess = (A_cnt == 4'd4);
+	assign KEY_3   = ~KEY_take;
+	assign LEDG[0] = ~KEY[0];
+	assign LEDG[3] = KEY_3;
 	
-	
-//	//========<測試>========
-//	assign LEDR[0] = KEY[3];
-//	assign LEDR[1] = clk18;
-//	//=====================
-	
+//========<狀態轉移>===========
 	always @(posedge clk18, negedge rst)begin
 		if(!rst)begin
 			state <= SET;
@@ -58,13 +57,14 @@ module guess(clk, rst, SW, HEX7, HEX6, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0, KEY, 
 		end
 	end
 	
+//========<檢測次一狀態/AB計算>========
 	integer x;
-	always @(*)begin
+	always @(*)begin 
 		case(state)
 			SET:begin
 				A_cnt = 4'd0;
 				B_cnt = 4'd0;
-				if(KEY[3] &&(`g3 !=`g2 && `g3 != `g1 && `g3 != `g0 && `g2 !=`g1 && `g2 != `g0 && `g1 != `g0))begin
+				if(KEY_3 &&(`g3 !=`g2 && `g3 != `g1 && `g3 != `g0 && `g2 !=`g1 && `g2 != `g0 && `g1 != `g0))begin
 					`A3 = `g3;
 					`A2 = `g2;
 					`A1 = `g1;
@@ -80,78 +80,37 @@ module guess(clk, rst, SW, HEX7, HEX6, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0, KEY, 
 				end
 			end
 			WAIT:begin
+				`A3 = `A3;
+				`A2 = `A2;
+				`A1 = `A1;
+				`A0 = `A0;
 				A_cnt = 4'd0;
 				B_cnt = 4'd0;
-				if(KEY[3])begin
-					if(|SW[15:0])begin
-						`A3 = `A3;
-						`A2 = `A2;
-						`A1 = `A1;
-						`A0 = `A0;
-						nextstate = WAIT;
-					end
-					else begin
-						`A3 = `A3;
-						`A2 = `A2;
-						`A1 = `A1;
-						`A0 = `A0;
-						nextstate = GUESS;
-					end
+				if(KEY_3)begin
+					nextstate = (|SW[15:0]) ? WAIT : GUESS;
 				end
 				else begin
-					`A3 = `A3;
-					`A2 = `A2;
-					`A1 = `A1;
-					`A0 = `A0;
 					nextstate = WAIT;
 				end
 			end
 			GUESS:begin
+				`A3 = `A3;
+				`A2 = `A2;
+				`A1 = `A1;
+				`A0 = `A0;
+				A_cnt = A_cnt;
+				B_cnt = B_cnt;
 				if(guess_cnt <= 18'b01_1111_1111_1111_1111)begin
-					if(KEY[3] && (`g3 !=`g2 && `g3 != `g1 && `g3 != `g0 && `g2 !=`g1 && `g2 != `g0 && `g1 != `g0))begin
-						`A3 = `A3;
-						`A2 = `A2;
-						`A1 = `A1;
-						`A0 = `A0;
-						A_cnt = 4'd0;
-						B_cnt = 4'd0;
-						A_cnt = A_cnt + (`A3 == `g3) + (`A2 == `g2) + (`A1 == `g1) + (`A0 == `g0);	
-						if(`g3 == `A2 || `g3 == `A1 || `g3 ==`A0)begin
-							B_cnt = B_cnt + + 1;
-						end
-						if(`g2 == `A3 || `g2 == `A1 || `g2 ==`A0)begin
-							B_cnt = B_cnt + + 1;
-						end
-						if(`g1 == `A3 || `g1 == `A2 || `g1 ==`A0)begin
-							B_cnt = B_cnt + + 1;
-						end
-						if(`g0 == `A3 || `g0 == `A2 || `g0 ==`A1)begin
-							B_cnt = B_cnt + + 1;
-						end
-						if(A_cnt == 4'd4)begin
-							nextstate = END;
-						end
-						else begin
-							nextstate = GUESS;
-						end
+					if(KEY_3 && (`g3 !=`g2 && `g3 != `g1 && `g3 != `g0 && `g2 !=`g1 && `g2 != `g0 && `g1 != `g0))begin
+						A_cnt = (`A3 == `g3) + (`A2 == `g2) + (`A1 == `g1) + (`A0 == `g0);	
+						B_cnt = (`g3 == `A2 || `g3 == `A1 || `g3 ==`A0) + (`g2 == `A3 || `g2 == `A1 || `g2 ==`A0) + (`g1 == `A3 || `g1 == `A2 || `g1 ==`A0) + (`g0 == `A3 || `g0 == `A2 || `g0 ==`A1); 
+						nextstate = (A_cnt == 4'd4) ? END : GUESS;
 					end
 					else begin
-						`A3 = `A3;
-						`A2 = `A2;
-						`A1 = `A1;
-						`A0 = `A0;
-						A_cnt = A_cnt;
-						B_cnt = B_cnt;
 						nextstate = GUESS;
 					end
 				end
 				else begin
-					`A3 = `A3;
-					`A2 = `A2;
-					`A1 = `A1;
-					`A0 = `A0;
-					A_cnt = A_cnt;
-					B_cnt = B_cnt;
 					nextstate = END;
 				end
 			end
@@ -162,12 +121,7 @@ module guess(clk, rst, SW, HEX7, HEX6, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0, KEY, 
 				`A0 = 4'd0;
 				A_cnt = A_cnt;
 				B_cnt = B_cnt;
-				if(KEY[3] &&(|SW[15:0])==0)begin
-					nextstate = SET;
-				end
-				else begin
-					nextstate = END;
-				end
+				nextstate = (KEY_3 &&(|SW[15:0])==0) ? SET : END;
 			end
 			default:begin
 				A_cnt = 4'd0;
@@ -181,6 +135,7 @@ module guess(clk, rst, SW, HEX7, HEX6, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0, KEY, 
 		endcase
 	end
 	
+//============<計數器>=======================
 	always @(posedge clk18, negedge rst)begin
 		if(!rst)begin
 			guess_cnt <= 18'd0;
@@ -250,7 +205,7 @@ module guess(clk, rst, SW, HEX7, HEX6, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0, KEY, 
 					{HEX2[0],HEX2[1],HEX2[2],HEX2[3],HEX2[4],HEX2[5],HEX2[6]} <= switch2;
 					{HEX1[0],HEX1[1],HEX1[2],HEX1[3],HEX1[4],HEX1[5],HEX1[6]} <= switch1;
 					{HEX0[0],HEX0[1],HEX0[2],HEX0[3],HEX0[4],HEX0[5],HEX0[6]} <= switch0;
-					if(KEY[3])begin
+					if(KEY_3)begin
 						if(`g3 !=`g2 && `g3 != `g1 && `g3 != `g0 && `g2 !=`g1 && `g2 != `g0 && `g1 != `g0)begin
 							{HEX7[0],HEX7[1],HEX7[2],HEX7[3],HEX7[4],HEX7[5],HEX7[6]} <= switch7;
 							{HEX6[0],HEX6[1],HEX6[2],HEX6[3],HEX6[4],HEX6[5],HEX6[6]} <= 7'b0001000;
@@ -303,6 +258,7 @@ module guess(clk, rst, SW, HEX7, HEX6, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0, KEY, 
 		end
 	end
 	
+//=========<轉圈計數器>=============
 	always @(posedge clk18, negedge rst)begin
 		if(!rst)begin
 			seg7_run_cycle <= 7'b0111111;
@@ -330,6 +286,7 @@ module guess(clk, rst, SW, HEX7, HEX6, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0, KEY, 
 		end
 	end
 endmodule 
+
 //==============<防彈跳>==================
 module KEY_Debounce(CLK, RST, KEY_In, KEY_Out); 
 	parameter DeB_Num = 4; 		// 取樣次數
@@ -391,6 +348,7 @@ module counterDivider(CLK, RST, CLK_Out);
 	
 endmodule
 
+//=========<開關解碼器>============
 module switch(in_4,out_LED_7);
 	input [3:0] in_4;
 	output reg [6:0] out_LED_7;
